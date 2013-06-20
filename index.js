9b0d71284af53e9786da939f459bedea678d630e
jkroso <jkroso@gmail.com>
2013-06-20 20:28:13 +1200

    use a generalized implementation

--- index.js
+++ index.js
@@ -1,16 +1,34 @@
 
-var array = require('./array')
-  , object = require('./object')
+var map = require('map/async')
+
+module.exports = all
 
 /**
- * dispatch to the appropriate module
- *
- * @param {Object|Arraylike} x
- * @return {Result} new x
+ * unbox all arguments
+ * @return {Result} arguments
  */
 
-module.exports = function(obj){
-	return typeof obj.length == 'number'
-		? array(obj)
-		: object(obj)
+all.naked = all.args = function(){
+	return all(arguments)
 }
+
+/**
+ * unbox all values in `obj`
+ * 
+ * @param {Object|Array} obj
+ * @return {Result} for a new `obj`
+ */
+
+function all(obj){
+	return map(obj, identity)
+}
+
+/**
+ * its a feature of map that any returned
+ * values inside of a Result will be unboxed
+ * 
+ * @param {x} a
+ * @return {x}
+ */
+
+function identity(a){ return a}
